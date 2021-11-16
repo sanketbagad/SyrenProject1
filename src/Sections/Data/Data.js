ba5c59ef23caa769b8eb1e44f2a5859ee865d5d1
Sanket Bagad <alencolins@gmail.com>
2021-11-16 09:58:49 +0530

    data section

--- src/Sections/Data/Data.js
+++ src/Sections/Data/Data.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./data.css"
 
 const Data = () => {
     return (
-        <div>
-            Data Visualization
+        <div className='d'>
+            <div className='d-nav'>
+            <h2 className='d-title'><span><i class="fal fa-star"></i></span>Control Tower</h2>
+            {/* <p className='d-feed'>Send Feedback <span><i class="fal fa-comments"></i></span></p> */}
+            </div>
+           
         </div>
     )
 }

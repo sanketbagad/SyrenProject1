534496b3cc942968e37dad36cf36e4eb4055177b
Sanket Bagad <alencolins@gmail.com>
2021-11-16 11:14:55 +0530

    All basic structures completed

--- src/Components/Fapps/Fapps.js
+++ src/Components/Fapps/Fapps.js
@@ -3,8 +3,14 @@ import React from 'react'
 const Fapps = () => {
     return (
         <div>
-            fav apps
+        <div>
+            <span>i</span>
+            <h3>Connected Visibility</h3>
         </div>
+        <div>
+            <span>i</span>
+        </div>
+     </div>
     )
 }
 

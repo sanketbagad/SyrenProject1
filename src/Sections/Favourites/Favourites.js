eeac4e56132f0e2d2c63f591774ba3903aedfb33
Sanket Bagad <alencolins@gmail.com>
2021-11-16 09:41:46 +0530

    Fontawesome and fav sextion

--- src/Sections/Favourites/Favourites.js
+++ src/Sections/Favourites/Favourites.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import { Navbar, Nav } from 'react-bootstrap';
 const Favourites = () => {
     return (
-        <div>
-            Favourites
-        </div>
+        
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Favourites</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav>
+      <Nav.Link href="#deets">Send Feedback</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
     )
 }
 

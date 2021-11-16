bb78b617c6553a0222da22ccc6e59adc4056d83e
Sanket Bagad <alencolins@gmail.com>
2021-11-16 09:51:07 +0530

    Favoutites section

--- src/Sections/Favourites/Favourites.js
+++ src/Sections/Favourites/Favourites.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap';
+import "./fav.css"
+
 const Favourites = () => {
     return (
-        
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">Favourites</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav>
-      <Nav.Link href="#deets">Send Feedback</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+        <div className='f'>
+            <div className='f-nav'>
+            <h2 className='f-title'><span><i class="fal fa-star"></i></span> Favourite Apps</h2>
+            <p className='f-feed'>Send Feedback <span><i class="fal fa-comments"></i></span></p>
+            </div>
+           
+        </div>
     )
 }
 

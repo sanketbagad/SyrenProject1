ebd5fd68fd54d5bad6efd6e272a3c5a9443cda84
Sanket Bagad <alencolins@gmail.com>
2021-11-16 11:09:37 +0530

    setting up the basic structure

--- src/Components/Dv/Dv.js
+++ src/Components/Dv/Dv.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import "./dv.css"
 
 const Dv = () => {
     return (
         <div>
-            DV
+           <div>
+               <span>i</span>
+               <h3>Connected Visibility</h3>
+           </div>
+           <div>
+               <span>i</span>
+           </div>
+
+           <div>
+               <ul>
+                   <li>long label tool</li>
+                   <li>long label tool</li>
+               </ul>
+           </div>
+
+           <a>Learn More</a>
         </div>
     )
 }
